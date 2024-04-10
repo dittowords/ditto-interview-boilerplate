788eff4eaf652bc5abaace095ea8070e10e47e27
Reed Barnes <reed@dittowords.com>
2024-04-10 16:02:56 -0700

    simple working app

--- server.ts
+++ server.ts
@@ -1,14 +1,19 @@
 import express from "express";
+import cors from "cors";
 const app = express();
+app.use(cors());
 const PORT = 4000;
 
-let counter = 0;
-
-app.get("/", (req, res) => {
-  res.send("Hello world!");
+// Simple logging middleware
+app.use((req, res, next) => {
+  const date = new Date();
+  const time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  console.log(`${time} ${req.method} ${req.url}`);
+  next();
 });
 
-app.post("/counter", (req, res) => {
+let counter = 0;
+app.get("/counter", (req, res) => {
   counter++;
   res.send({ counter });
 });
